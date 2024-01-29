c022475ab903006052ca1803c03082319f25dd33
Enzo Buscaglia <embuscagliah@gmail.com>
2024-01-28 21:59:54 -0300

    change name to function, adjust serializer accordingly

--- budapi/utils.py
+++ budapi/utils.py
@@ -2,7 +2,7 @@ import requests
 from django.conf import settings
 
 
-def get_markets_ids():
+def get_every_market_id():
     response = requests.get(f"{settings.BUDA_API_URL}/markets")
     if response.status_code == 200:
         market_ids = []
