a108670f3ea1f77e642dbbb41690cdbf4c8c1339
Enzo Buscaglia <embuscagliah@gmail.com>
2024-01-29 17:42:53 -0300

    add polling feature to APIView

--- budapi/views.py
+++ budapi/views.py
@@ -8,8 +8,10 @@ from budapi.serializers import (
     SecretKeyGetRequestSerializer,
     SingleMarketSpreadGetReponseSerializer,
     SingleMarketSpreadGetRequestSerializer,
+    SingleMarketSpreadPostRequestSerializer,
 )
 from budapi.services.base import calculate_single_market_spread, get_all_market_spreads
+from model.models import MarketSpreadAlert
 
 
 class SingleMarketSpread(APIView):
@@ -28,7 +30,7 @@ class SingleMarketSpread(APIView):
         )
         if market_spread is None:
             return Response(
-                {"error": "External API Buda API not responding"},
+                {"error": "External Buda API not responding"},
                 status=status.HTTP_502_BAD_GATEWAY,
             )
         response_data = {
@@ -41,6 +43,69 @@ class SingleMarketSpread(APIView):
             return Response(response_serializer.validated_data, status.HTTP_200_OK)
         return Response(
             response_serializer.errors, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
+
+    def post(self, request):
+        request_serializer = SingleMarketSpreadPostRequestSerializer(data=request.data)
+        if not request_serializer.is_valid():
+            return Response(
+                request_serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )
+        alert_spread, alert_spread_created = MarketSpreadAlert.objects.update_or_create(
+            market_id=request_serializer.validated_data["market_id"],
+            defaults={
+                "alert_spread": request_serializer.validated_data["alert_spread"],
+                "trading_currency": request_serializer.validated_data[
+                    "trading_currency"
+                ],
+            },
+        )
+        if not alert_spread:
+            return Response(
+                {"error": "No spread alert found or created"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        return Response(
+            {"alert_spread_created": alert_spread_created},
+            status=(
+                status.HTTP_201_CREATED if alert_spread_created else status.HTTP_200_OK
+            ),
+        )
+
+
+class SingleMarketSpreadPoll(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        request_serializer = SingleMarketSpreadGetRequestSerializer(
+            data=request.query_params
+        )
+        if not request_serializer.is_valid():
+            return Response(
+                request_serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )
+        current_market_spread, _ = calculate_single_market_spread(
+            request_serializer.validated_data["market_id"]
+        )
+        if current_market_spread is None:
+            return Response(
+                {"error": "External Buda API not responding"},
+                status=status.HTTP_502_BAD_GATEWAY,
+            )
+        market_alert_spread = MarketSpreadAlert.objects.filter(
+            market_id=request_serializer.validated_data["market_id"]
+        )
+        if not market_alert_spread:
+            return Response(
+                {"error": "There is no alert_spread set for that market"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+        return Response(
+            {
+                "current_spread_above_alert": current_market_spread
+                > market_alert_spread[0].alert_spread
+            },
+            status.HTTP_200_OK,
         )
 
 
