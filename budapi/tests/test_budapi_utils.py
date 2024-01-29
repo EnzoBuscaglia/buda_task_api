4f664ba442e1aff155697ac2a77403c5af05969c
Enzo Buscaglia <embuscagliah@gmail.com>
2024-01-29 18:49:34 -0300

    added budapi_utils test with mock

---
+++ budapi/tests/test_budapi_utils.py
@@ -0,0 +1,13 @@
+from unittest.mock import patch
+from django.test import TestCase
+from budapi.utils import get_every_public_market_id
+
+class TestBudapiUtils(TestCase):
+    @patch("budapi.utils.get_every_public_market_id")
+    def test_get_every_public_market_id(self, mock_get_every_public_market_id):
+        mock_get_every_public_market_id.return_value = [
+          "btc-clp", "btc-cop", "eth-clp", "eth-btc", "btc-pen", "eth-pen", "eth-cop", "bch-btc", "bch-clp", "bch-cop", "bch-pen", "btc-ars", "eth-ars", "bch-ars", "ltc-btc", "ltc-clp", "ltc-cop", "ltc-pen", "ltc-ars", "usdc-clp", "usdc-cop", "usdc-pen", "usdc-ars", "btc-usdc", "usdt-usdc"
+        ]
+        market_ids = get_every_public_market_id()
+        self.assertIn("btc-clp", market_ids)
+        self.assertIn("ltc-cop", market_ids)
