4a4cbde187bb993030d6813c895103601f2ed665
Enzo Buscaglia <embuscagliah@gmail.com>
2024-01-29 23:39:11 -0300

    refactor base to include timer in case api response fails at first attempt

--- budapi/services/base.py
+++ budapi/services/base.py
@@ -1,3 +1,5 @@
+import time
+
 import requests
 from django.conf import settings
 
@@ -5,15 +7,21 @@ from budapi.utils import get_every_public_market_id
 
 
 def calculate_single_market_spread(market_id):
-    url = f"{settings.BUDA_API_URL}/markets/{market_id}/ticker"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json().get("ticker", [])
-        if data:
-            return (
-                float(data["min_ask"][0]) - float(data["max_bid"][0]),
-                data["min_ask"][1].lower(),
+    for i in range(3):
+        time.sleep(i * 3)
+        try:
+            response = requests.get(
+                f"{settings.BUDA_API_URL}/markets/{market_id}/ticker"
             )
+            if response.status_code == 200:
+                data = response.json().get("ticker", [])
+                if data:
+                    return (
+                        float(data["min_ask"][0]) - float(data["max_bid"][0]),
+                        data["min_ask"][1].lower(),
+                    )
+        except Exception:
+            continue
     return None, None
 
 
